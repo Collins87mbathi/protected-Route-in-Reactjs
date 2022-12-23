0b3153658ad34e8953f6f7a6013f570e590d03ab
collins mbathi <perezcoolins@gmail.com>
2022-12-23 13:01:35 +0300

    fixed the redux side of fetching

---
+++ src/Redux/Slice/Slice.js
@@ -0,0 +1,30 @@
+import  {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    
+    state: {
+        isFetching: false,
+    },
+    user:{
+  name:"collins",
+  isAuthenticated:true
+},
+}
+
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setIsFetching : (state) => {
+        state.state.isFetching = true;
+  }, 
+  }  
+});
+
+export const {
+      setIsFetching,
+    } = userSlice.actions;
+
+
+export default userSlice.reducer;
---
+++ src/utils/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useSelector} from "react-redux"
+import {Navigate, useLocation} from "react-router-dom"
+
+const ProtectedRoute = ({children}) => {
+    const user = useSelector((state) => state.user);
+    let location = useLocation();
+
+    if(!user.state.isAuthenticated) {
+        return <Navigate to="/login" state={{ from: location}} replace />
+    }
+ return children
+  
+};
+
+export default ProtectedRoute;
